6410d987bf6933f60e6cb282eb5947d11a558e0f
tristan <tristan@busch-germany.de>
2021-12-16 19:57:19 +0100

    screenmanager

---
+++ MyProject/ScreenManager.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace MyProject
+{
+    class ScreenManager
+    {
+        public ScreenManager ()
+        {
+
+        }
+    }
+
+    public interface IScreen
+    {
+        bool DrawLower { get; }
+
+        bool UpdateLower { get; }
+
+        public void Draw(SpriteBatch sprite) { }
+
+        public void Update() { }
+    }
+}
